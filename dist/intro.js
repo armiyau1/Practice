81040f0aa485dd254d48f3832c3cbcf860d3bfca
armiyau1 <yauarmi688@gmail.com>
2025-03-18 14:11:33 +0100

    changes

--- dist/intro.js
+++ dist/intro.js
@@ -1,23 +1,23 @@
-var car = /** @class */ (function () {
-    function car(brand, model, year) {
+"use strict";
+class car {
+    constructor(brand, model, year) {
         this.brand = brand;
         this.model = model;
         this.year = year;
     }
-    car.prototype.getCarInfo = function () {
-        return "".concat(this.brand, " ").concat(this.model, " ").concat(this.brand);
-    };
-    car.prototype.getCarAge = function () {
+    getCarInfo() {
+        return `${this.brand} ${this.model} ${this.year}`;
+    }
+    getCarAge() {
         if (this.year < 2015) {
             console.log("your car is old");
         }
         else {
             console.log("your car is new");
         }
-    };
-    return car;
-}());
-var myCar = new car("Toyota", "Camry", 2010);
+    }
+}
+const myCar = new car("Toyota", "Camry", 2010);
 myCar.getCarAge();
-var nyCar = new car("Toyota", "Camry", 2022);
-myCar.getCarAge();
+const nyCar = new car("Toyota", "Camry", 2022);
+nyCar.getCarAge();
